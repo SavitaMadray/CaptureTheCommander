ad0b5cd3dc8a8a5e6db74cb9614c3f0621bb7f82
your name here <your_email_here>
2020-04-09 00:28:53 -0400

    added deck and some styling

--- capturethecommander/src/App.js
+++ capturethecommander/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import CardGame from "./Component/cardgame.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <CardGame></CardGame>
     </div>
   );
 }
